61f6a5f02e8a97c0aea0ae7958718d80fceeda13
U-frank-PC\frank <frank@frank-PC.(none)>
2013-05-26 02:58:34 +0800

    新增phpcms框架

---
+++ caches/configs/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'database' => 'content_wap',
+		'username' => 'root',
+		'password' => '',
+		'tablepre' => 'wap_',
+		'charset' => 'utf8',
+		'type' => 'mysql',
+		'debug' => true,
+		'pconnect' => 1,
+		'autoconnect' => 0
+		),
+);
+
+?>
---
+++ phpsso_server/caches/configs/database.php
@@ -0,0 +1,18 @@
+<?php
+
+return array (
+	'default' => array (
+		'hostname' => 'localhost',
+		'database' => 'content_wap',
+		'username' => 'root',
+		'password' => '',
+		'tablepre' => 'wap_sso_',
+		'charset' => 'utf8',
+		'type' => 'mysql',
+		'debug' => true,
+		'pconnect' => 1,
+		'autoconnect' => 0
+		)
+);
+
+?>
